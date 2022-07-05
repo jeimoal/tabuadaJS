d12f543921db76e0fa49c7f83053d77d59fddff1
Jeison Moura <85846383+jeimoal@users.noreply.github.com>
2022-07-05 14:15:08 -0300

    Update tabuadaJS.js
    Adição das quatro operações básicas

--- tabuadaJS.js
+++ tabuadaJS.js
@@ -1,31 +1,66 @@
 const numeroDigitado = document.getElementById('numeroDigitado');
-const resultados = document.getElementById('resultados');
 const btnEnviar = document.getElementById('enviar');
 const btnLimpar = document.getElementById('limpar');
+
+const resultSoma = document.getElementById('resultSoma');
+const resultSub = document.getElementById('resultSub');
+const resultMult = document.getElementById('resulMult');
+const resultDiv = document.getElementById('resulDiv');
+
 	
-let multiplicacao;
+let tabelaResult;
 
 btnEnviar.addEventListener('click', tabuada);
 
 function tabuada() {
 	let numDigi = numeroDigitado.value;
-	let resultMulti = resultados;
 	let total;
 
-	for(i = 1; i <= 10; i++) {
-		total = numDigi * i;
-		multiplicacao = document.createElement('p');
-		multiplicacao.textContent = total;
-		resultados.appendChild(multiplicacao);
-	}
-	btnLimpar.disabled = false;
+	//cálculo da soma
+	let paraSoma = resultSoma;
+	for(contSum = 1; contSum <= 10; contSum++) {
+		total = Number(numDigi) + contSum;
+		tabelaResult = document.createElement('p');
+		tabelaResult.textContent = `${numDigi} + ${contSum} = ${total}`;
+		paraSoma.appendChild(tabelaResult);
+		}
+
+	//cálculo subtração
+	let paraSub = resultSub;
+	for(contSub = 1; contSub <= 10; contSub++) {
+		total = Number(numDigi) - contSub;
+		tabelaResult = document.createElement('p');
+		tabelaResult.textContent = `${numDigi} - ${contSub} = ${total}`;
+		paraSub.appendChild(tabelaResult);
+		}
+
+	//cálculo multiplicação
+	let paraMult = resultMult;
+	for(contMult = 1; contMult <= 10; contMult++) {
+		total = Number(numDigi) * contMult;
+		tabelaResult = document.createElement('p');
+		tabelaResult.textContent = `${numDigi} * ${contMult} = ${total}`;
+		paraMult.appendChild(tabelaResult);
+		}
+
+	//cálculo divisão
+	let paraDiv = resulDiv;
+	for(contDiv = 1; contDiv <= 10; contDiv++) {
+		total = Number(numDigi) / contDiv;
+		total = total.toFixed(2);
+		tabelaResult = document.createElement('p');
+		tabelaResult.textContent = `${numDigi} / ${contDiv} = ${total}`;
+		paraDiv.appendChild(tabelaResult);
+		}
+
 	numeroDigitado.value = "";
+	btnLimpar.disabled = false;
 }
 
 btnLimpar.addEventListener('click', limparTabuada);
 
 function limparTabuada() {
-	let limpeza = document.querySelectorAll('.resultados p')
+	let limpeza = document.querySelectorAll('#tabuada p')
 	for(i = 0; i < limpeza.length; i++) {
 		limpeza[i].textContent = "";
 	}
